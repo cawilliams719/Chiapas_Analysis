2176b83f9e26df6b29ebda1e269855b2cf851a13
cwilliams_risk_01 <cawilliams719@gmail.com>
2023-12-18 23:09:36 +0000

    Iterate through 1+ models

--- precip_analysis.R
+++ precip_analysis.R
@@ -6,194 +6,189 @@
 
 # Import libraries
 library(stars)
-library(dplyr)
-library(stringr)
-library(ggplot2)
+library(tidyverse)
 library(ggthemes)
 library(viridis)
 library(viridisLite)
+library(cowplot)
+library(furrr)
+
 
 ################################################################################
-# # Copy files from cmip6_data to persistent drive
-# ## Set source folder and destination folder paths
-# from_dir <- "/home/cwilliams/cmip6_directory/ISIMIP_BASD_data/daily/precipitation/ssp245/"
-# to_dir <- "/home/cwilliams/persistent_disk/chiapas/precip/data/"
+# Copy files from cmip6_data to persistent drive
+## Set source folder and destination folder paths
+from_dir <- "/home/cwilliams/cmip6_directory/kgassert_TEMP/Chiapas/pr"
+# Set working directory for data downloads and reading in files
+working_dir <- "/home/cwilliams/persistent_disk/chiapas/precip/data/"
+
+## Set file pattern to detect in from_dir
+pattern <- "Chiapas_pr_ACCESS"
+
+# ###############################################################################
+# ## List and copy files from cmip6_data bucket to persistent drive
+# from_dir %>%
+#   list.files(full.names = TRUE, pattern = pattern) %>%
+#   map(function(f) {
+#     f_gs <- str_replace(f, "/home/cwilliams/cmip6_directory", "gs://cmip6_data")
 # 
-# ## Set file pattern to detect in from_dir
-# pattern <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_20[0-5][0-9].nc"
-# 
-# ## Save list of file names to copy
-# the_files <- list.files(path = from_dir, pattern = pattern)
-# 
-# ## Copy files to to_dir
-# file.copy(from = file.path(from_dir, the_files),
-#                     to = file.path(to_dir, the_files))
-################################################################################
+#     str_glue("gsutil cp {f_gs} {working_dir}") %>%
+#       system()
+#   })
+# ###############################################################################
 
 # Read and Pre-process Data
 ## Set Chiapas extent
-bb = st_bbox(c(xmin = -95.1,
-               ymin = 13.5,
-               xmax = -89.4,
-               ymax = 19.0))
+# bb = st_bbox(c(xmin = -95.1,
+#                ymin = 13.5,
+#                xmax = -89.4,
+#                ymax = 19.0))
   
-## Set working directory to read in files
-working_dir <- "/home/cwilliams/persistent_disk/chiapas/precip/data/"
-
-## Set pattern for list.files
-model_name_00s <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_200[0-9].nc"
-model_name_10s <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_201[0-9].nc"
-model_name_20s <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_202[0-9].nc"
-model_name_30s <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_203[0-9].nc"
-model_name_40s <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_204[0-9].nc"
-model_name_50s <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_205[0-9].nc"
-model_name_60 <- "pr_ACCESS-CM2_ssp245_basd_0.5deg_2060.nc"
-
-# List files based on directory and file nomenclature pattern
-precip_list_00s = list.files(path = working_dir, pattern = model_name_00s, full.names = TRUE)
-precip_list_10s = list.files(path = working_dir, pattern = model_name_10s, full.names = TRUE)
-precip_list_20s = list.files(path = working_dir, pattern = model_name_20s, full.names = TRUE)
-precip_list_30s = list.files(path = working_dir, pattern = model_name_30s, full.names = TRUE)
-precip_list_40s = list.files(path = working_dir, pattern = model_name_40s, full.names = TRUE)
-precip_list_50s = list.files(path = working_dir, pattern = model_name_50s, full.names = TRUE)
-precip_list_60 = list.files(path = working_dir, pattern = model_name_60, full.names = TRUE)
-
-## Read in NetCDF cmip6 data & crop to Chiapas extent
-chiapas_2000s <- precip_list_00s %>% read_stars(.) %>% st_crop(.,  bb, crop = TRUE)
-chiapas_2010s <- precip_list_10s %>% read_stars(.) %>% st_crop(.,  bb, crop = TRUE)
-chiapas_2020s <- precip_list_20s %>% read_stars(.) %>% st_crop(.,  bb, crop = TRUE)
-chiapas_2030s <- precip_list_30s %>% read_stars(.) %>% st_crop(.,  bb, crop = TRUE)
-chiapas_2040s <- precip_list_40s %>% read_stars(.) %>% st_crop(.,  bb, crop = TRUE)
-chiapas_2050s <- precip_list_50s %>% read_stars(.) %>% st_crop(.,  bb, crop = TRUE)
-chiapas_2060 <- precip_list_60 %>% read_stars(.) %>% st_crop(.,  bb, crop = TRUE)
-
-## Combine into one stars object
-chiapas <- c(chiapas_2000s, chiapas_2010s, chiapas_2020s, chiapas_2030s,
-             chiapas_2040s, chiapas_2050s, chiapas_2060)
-
-## Remove initial files
-rm(chiapas_2000s, chiapas_2010s, chiapas_2020s, chiapas_2030s, chiapas_2040s, 
-   chiapas_2050s, chiapas_2060)
+# Read in precipitation data
+precip <- working_dir %>% 
+  list.files(full.names = TRUE, pattern = pattern) %>% 
+  map(function(f) {
+    read_ncdf(f, proxy = FALSE)
+  }) 
 
 # Calculate Annual Precipitation (Total)
-# ## Monthly Mean
-# monthly_mean <- chiapas %>% aggregate(., by = "1 month", FUN = mean) %>% 
-#   aperm(., c("x", "y", "time")) 
-# 
-# ## Annual 
-# ## Sum of Monthly Mean
-# total_annual_mean <- monthly_mean %>% aggregate(., by = "1 year", FUN = sum) %>% 
-#   aperm(., c("x", "y", "time")) 
-
 ## Sum from Daily
-total_annual <- chiapas %>% aggregate(., by = "1 year", FUN = sum) %>% 
-  aperm(., c("x", "y", "time")) 
+total_annual <- precip[1:length(precip)] %>% 
+  map(function(f) {
+    filter(f, time >= "2001-01-01", time < "2061-01-01") %>% 
+      aggregate(by = "1 year", FUN = sum) 
+  }) 
 
-# Annual Means per Time Period
+# Annual Precipitation (Total) Means per Time Period
 ## 2001-2020
-mean_total_annual_1 <- total_annual %>% 
-  filter(time < "2021-01-01") %>% 
-  aggregate(., by = "20 years", FUN = mean) %>% 
-  aperm(., c("x", "y", "time")) 
+mean_total_annual_1 <- total_annual[1:length(total_annual)] %>%
+  map(function(f) {
+    filter(f, time < "2021-01-01") %>% 
+      aggregate(by = "20 years", FUN = mean) %>%
+      adrop()
+  })
 
 ## 2021-2040
-mean_total_annual_2 <- total_annual %>% 
-  filter(time >"2020-01-01", time < "2041-01-01") %>% 
-  aggregate(., by = "20 years", FUN = mean) %>% 
-  aperm(., c("x", "y", "time")) 
+mean_total_annual_2 <- total_annual[1:length(total_annual)] %>% 
+  map(function(f) {
+    filter(f, time >"2020-01-01", time < "2041-01-01") %>% 
+      aggregate(by = "20 years", FUN = mean) %>%
+      adrop()
+  })
 
 ## 2041-2060
-mean_total_annual_3 <- total_annual %>% 
-  filter(time > "2040-01-01") %>% 
-  aggregate(., by = "20 years", FUN = mean) %>% 
-  aperm(., c("x", "y", "time")) 
+mean_total_annual_3 <- total_annual[1:length(total_annual)] %>% 
+  map(function(f) {
+    filter(f, time > "2040-01-01") %>% 
+      aggregate(by = "20 years", FUN = mean) %>% 
+      adrop()
+  })
 
 # Change in Precipitation
-## Subtract Function
-subtract <- function(x) (x[[2]]-x[[1]]) 
-
 ## Change Period 2-1: (2021-2040) - (2001-2020)
-mean_total_annual_1_tchange <- st_set_dimensions(mean_total_annual_1, "time", as.POSIXct("2020-01-01")) # Reset time for the first period to 2020 so when aggregating with time period 2 it is only 1 year difference
-change1 <- c(mean_total_annual_1_tchange, mean_total_annual_2) # combine two time periods to one star object
-st_get_dimension_values(change1, "time") # check dates
+subtracted1 <- mapply("-", mean_total_annual_2, mean_total_annual_1, SIMPLIFY = FALSE)
 
-
-subtracted1 <- change1 %>% aggregate(., by = "2 years", FUN = subtract) %>%
-  aperm(., c("x", "y", "time"))
+# Test output is correct
+x <- subtracted1[[1]]
+y <- mean_total_annual_2[[1]]-mean_total_annual_1[[1]]
+x$pr-y$pr # all output pixels are 0 or NA
 
 ## Change Period 3-1: (2041-2060) - (2001-2020)
-mean_total_annual_1_tchange2 <- st_set_dimensions(mean_total_annual_1, "time", as.POSIXct("2040-01-01")) # Reset time again
-change2 <- c(mean_total_annual_1_tchange2, mean_total_annual_3) # combine two time periods to one star object
-st_get_dimension_values(change2, "time") # check dates
-
-
-subtracted2 <- change2 %>% aggregate(., by = "2 years", FUN = subtract) %>%
-  aperm(., c("x", "y", "time"))
+subtracted2 <- mapply("-", mean_total_annual_3, mean_total_annual_1, SIMPLIFY = FALSE)
 
 # Percent Change in Precipitation
 ## Percent Change Function
-perchange <- function(x) (((x[[2]]-x[[1]])/x[[1]])*100) # double check
+perchange <- function(i, j) (((j-i)/i)*100) 
 
 ## Percent Change Period 2-1: (2021-2040) - (2001-2020)
-perchangeim1 <- change1 %>% aggregate(., by = "2 years", FUN = perchange) %>%
-  aperm(., c("x", "y", "time"))
+perchangeim1 <- mapply(perchange, mean_total_annual_1, mean_total_annual_2, SIMPLIFY = FALSE)
+
+# Test output is correct
+a <- perchangeim1[[1]]
+b <- (((mean_total_annual_2[[1]]-mean_total_annual_1[[1]])/mean_total_annual_1[[1]])*100)
+a$pr-b$pr # all output pixels are 0 or NA
 
 ## Percent Change Period 3-1: (2041-2060) - (2001-2020)
-perchangeim2 <- change2 %>% aggregate(., by = "2 years", FUN = perchange) %>%
-  aperm(., c("x", "y", "time"))
+perchangeim2 <- mapply(perchange, mean_total_annual_1, mean_total_annual_2, SIMPLIFY = FALSE)
+
+################################################################################
+
+# Visualize outputs - Testing and updating maps
+
+# TESTING - 2 models x 2 difference maps
+s1 <- subtracted1 %>% 
+  map(function(f) {
+    ggplot() +  
+      geom_stars(data = f) + 
+      scale_fill_viridis_c(option = "D") +
+      coord_equal() +
+      theme_map() +
+      labs(subtitle = "(2021-2040) - (2001-2020)") 
+  })
+s2 <- subtracted2 %>% 
+  map(function(f) {
+    ggplot() +  
+      geom_stars(data = f) + 
+      scale_fill_viridis_c(option = "D") +
+      coord_equal() +
+      theme_map() +
+      labs(subtitle = "(2041-2060) - (2001-2020)") 
+  })
+
+plot_grid(s1[[1]], s1[[2]],
+          s2[[1]], s2[[2]],
+          nrow = 2, ncol = 2, 
+          labels = "AUTO") 
 
 
-# Visualize outputs
-
-## Average Total Precipitation: 2001-2020, 2021-2040, 2041-2060
-### Change time labels for visuals
-time_labels <- c("2001-2020", "2021-2040", "2041-2060")
-names(time_labels) <- c("2001-01-01", "2021-01-01", "2041-01-01")
-
-### Map Annual Total Precipitation
-ggplot() +  
-  geom_stars(data = c(mean_total_annual_1, mean_total_annual_2, mean_total_annual_3)) + 
-  facet_wrap("time", labeller = labeller("time" = time_labels)) +
-  scale_fill_viridis_c(option = "D") +
-  coord_equal() +
-  theme_map() +
-  ncol(3) +
-  nrow(1) +
-  # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
-  labs(title = "Total Annual Precipitation") +
-  theme(legend.position = "bottom") +
-  theme(legend.key.width = unit(1.5, "cm"))
-
-### Change in Precipitation: 2-1 & 3-1
-### Change time labels for visuals
-time_labels_change <- c("(2021-2040) - (2001-2020)", "(2041-2060) - (2001-2020)")
-names(time_labels_change) <- c("2020-01-01", "2040-01-01")
-
-### Map Change in Precipitation
-ggplot() +  
-  geom_stars(data = c(subtracted1, subtracted2)) + 
-  facet_wrap("time", labeller = labeller("time" = time_labels_change)) +
-  scale_fill_viridis_c(option = "D") +
-  coord_equal() +
-  theme_map() +
-  ncol(2) +
-  nrow(1) +
-  # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
-  labs(title = "Change (mm) in Precipitation") +
-  theme(legend.position = "bottom") +
-  theme(legend.key.width = unit(1.5, "cm"))
-
-## Percent Change in Precipitation: 2-1 & 3-1
-### Map Change in Precipitation
-ggplot() +  
-  geom_stars(data = c(perchangeim1, perchangeim2)) + 
-  facet_wrap("time", labeller = labeller("time" = time_labels_change)) +
-  scale_fill_viridis_c(option = "D") +
-  coord_equal() +
-  theme_map() +
-  ncol(2) +
-  nrow(1) +
-  # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
-  labs(title = "Change (%) in Precipitation") +
-  theme(legend.position = "bottom") +
-  theme(legend.key.width = unit(1.5, "cm"))
+# ## Average Total Precipitation: 2001-2020, 2021-2040, 2041-2060
+# ### Change time labels for visuals
+# time_labels <- c("2001-2020", "2021-2040", "2041-2060")
+# names(time_labels) <- c("2001-01-01", "2021-01-01", "2041-01-01")
+# 
+# ### Map Annual Total Precipitation
+# ggplot() +  
+#   geom_stars(data = c(mean_total_annual_1, mean_total_annual_2, mean_total_annual_3)) + 
+#   facet_wrap("time", labeller = labeller("time" = time_labels)) +
+#   scale_fill_viridis_c(option = "D") +
+#   coord_equal() +
+#   theme_map() +
+#   ncol(3) +
+#   nrow(1) +
+#   # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
+#   labs(title = "Average Total Annual Precipitation") +
+#   theme(legend.position = "bottom") +
+#   theme(legend.key.width = unit(1.5, "cm"))
+# 
+# ## Change in Precipitation: 2-1 & 3-1
+# ### Change time labels for visuals
+# time_labels_change <- c("(2021-2040) - (2001-2020)", "(2041-2060) - (2001-2020)")
+# names(time_labels_change) <- c("2020-01-01", "2040-01-01")
+# 
+# ### Map Change in Precipitation
+# ggplot() +  
+#   geom_stars(data = c(subtracted1, subtracted2)) + 
+#   facet_wrap("time", labeller = labeller("time" = time_labels_change)) +
+#   scale_fill_viridis_c(option = "D") +
+#   coord_equal() +
+#   theme_map() +
+#   ncol(2) +
+#   nrow(1) +
+#   # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
+#   labs(title = "Change (mm) in Precipitation") +
+#   theme(legend.position = "bottom") +
+#   theme(legend.key.width = unit(1.5, "cm"))
+# 
+# ## Percent Change in Precipitation: 2-1 & 3-1
+# ### Map Percent Change in Precipitation
+# ggplot() +  
+#   geom_stars(data = c(perchangeim1, perchangeim2)) + 
+#   facet_wrap("time", labeller = labeller("time" = time_labels_change)) +
+#   scale_fill_viridis_c(option = "D") +
+#   coord_equal() +
+#   theme_map() +
+#   ncol(2) +
+#   nrow(1) +
+#   # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
+#   labs(title = "Change (%) in Precipitation") +
+#   theme(legend.position = "bottom") +
+#   theme(legend.key.width = unit(1.5, "cm"))
