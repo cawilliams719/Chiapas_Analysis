6bfb7f0701845eae0ddc70a14912622c34045b7c
cwilliams_risk_01 <cawilliams719@gmail.com>
2023-12-28 17:19:48 +0000

    updated plots

--- precip_analysis.R
+++ precip_analysis.R
@@ -96,7 +96,7 @@ b <- (((mean_total_annual_2[[1]]-mean_total_annual_1[[1]])/mean_total_annual_1[[
 a$pr-b$pr # all output pixels are 0 or NA
 
 ## Percent Change Period 3-1: (2041-2060) - (2001-2020)
-perchangeim2 <- mapply(perchange, mean_total_annual_1, mean_total_annual_2, SIMPLIFY = FALSE)
+perchangeim2 <- mapply(perchange, mean_total_annual_1, mean_total_annual_3, SIMPLIFY = FALSE)
 
 
 # Precipitation Extremes
@@ -204,7 +204,7 @@ GEV_test <- ecdf_1[1:length(ecdf_1)] %>%
 # Visualize outputs
 ## View and select model for all map precipitation outputs
 names(precip) # Examine list of models in console
-plot_model <- names(precip[2]) # Select index number corresponding to model of interest or replace with model string
+plot_model <- names(precip[1]) # Select index number corresponding to model of interest or replace with model string
 
 ## Average Total Precipitation: 2001-2020, 2021-2040, 2041-2060
 ### Subtitle time  labels for visuals
@@ -213,81 +213,157 @@ time_labels <- c("2001-2020", "2021-2040", "2041-2060")
 ### Map Annual Total Precipitation
 #### Map with legend first to call later separately
 mta_legend <- map(c(mean_total_annual_1[plot_model], 
-           mean_total_annual_2[plot_model], 
-           mean_total_annual_3[plot_model]), 
-         function(p) {
-           ggplot() + 
-             geom_stars(data = p) +
-             scale_fill_viridis_c(option = "D", na.value = "transparent") +
-             coord_equal() +
-             labs(fill = "Precipitation") +
-             theme(legend.position = "bottom") +
-             theme(legend.key.width = unit(1.5, 'cm'))
-           })
+                    mean_total_annual_2[plot_model], 
+                    mean_total_annual_3[plot_model]), 
+                  function(p) {
+                    ggplot() + 
+                      geom_stars(data = p) +
+                      scale_fill_viridis_c(option = "D", na.value = "transparent") +
+                      coord_equal() +
+                      labs(fill = "Precipitation") +
+                      theme(legend.position = "bottom") +
+                      theme(legend.key.width = unit(1.5, 'cm'))
+                  })
+
 #### Map without legend that will later have single legend added
 mta_nolegend <- map(c(mean_total_annual_1[plot_model], 
-            mean_total_annual_2[plot_model], 
-            mean_total_annual_3[plot_model]), 
-         function(p) {
-           ggplot() + 
-             geom_stars(data = p) +
-             scale_fill_viridis_c(option = "D", na.value = "transparent") +
-             coord_equal() +
-             theme(legend.position = "none")
-           })
+                      mean_total_annual_2[plot_model], 
+                      mean_total_annual_3[plot_model]), 
+                    function(p) {
+                      ggplot() + 
+                        borders("world", fill = "gray", colour = "black") +
+                        geom_stars(data = p) +
+                        scale_fill_viridis_c(option = "D", na.value = "transparent") +
+                        coord_equal(xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
+                        theme_light() +
+                        theme(legend.position = "none")
+                    })
 
 #### Add time ranges as labels to no legend map
 p_mta_nolegend <- plot_grid(plotlist = mta_nolegend, 
-               nrow = 1, 
-               ncol = 3, 
-               labels = time_labels,
-               label_size = 12) 
+                            nrow = 1, 
+                            ncol = 3, 
+                            labels = time_labels,
+                            label_size = 12) 
 
-#### Add legend and modify postion
+#### Add legend and modify position
 p_mta_legend <- plot_grid(p_mta_nolegend + theme(legend.position = "none"), 
-                get_legend(mta_legend[[1]] + theme(legend.box.margin = margin(20, 100, 80, 60))),
-                rel_heights = c(0.8, 0.2), 
-                nrow = 2, 
-                ncol = 1)
+                          get_legend(mta_legend[[1]] + theme(legend.box.margin = margin(20, 100, 80, 60))),
+                          rel_heights = c(0.8, 0.2), 
+                          nrow = 2, 
+                          ncol = 1)
 
 #### Create new ggplot with overarching title and subtitle
-title_gg <- ggplot() + 
+title_gg1 <- ggplot() + 
   labs(title = "Average Total Annual Precipitation", subtitle = str_glue("Model: {plot_model}")) 
 
 #### Final plot for Average Total Annual precipitation
-plot_grid(title_gg, p2, ncol = 1, rel_heights = c(0.15, 1))
+mta_map <- plot_grid(title_gg1, p_mta_legend, ncol = 1, rel_heights = c(0.15, 1))
+mta_map
 
 
-# ## Change in Precipitation: 2-1 & 3-1
-# ### Change time labels for visuals
-# time_labels_change <- c("(2021-2040) - (2001-2020)", "(2041-2060) - (2001-2020)")
-# names(time_labels_change) <- c("2020-01-01", "2040-01-01")
-# 
-# ### Map Change in Precipitation
-# ggplot() +  
-#   geom_stars(data = c(subtracted1, subtracted2)) + 
-#   facet_wrap("time", labeller = labeller("time" = time_labels_change)) +
-#   scale_fill_viridis_c(option = "D") +
-#   coord_equal() +
-#   theme_map() +
-#   ncol(2) +
-#   nrow(1) +
-#   # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
-#   labs(title = "Change (mm) in Precipitation") +
-#   theme(legend.position = "bottom") +
-#   theme(legend.key.width = unit(1.5, "cm"))
-# 
-# ## Percent Change in Precipitation: 2-1 & 3-1
-# ### Map Percent Change in Precipitation
-# ggplot() +  
-#   geom_stars(data = c(perchangeim1, perchangeim2)) + 
-#   facet_wrap("time", labeller = labeller("time" = time_labels_change)) +
-#   scale_fill_viridis_c(option = "D") +
-#   coord_equal() +
-#   theme_map() +
-#   ncol(2) +
-#   nrow(1) +
-#   # borders("world", xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
-#   labs(title = "Change (%) in Precipitation") +
-#   theme(legend.position = "bottom") +
-#   theme(legend.key.width = unit(1.5, "cm"))
+## Change in Precipitation: 2-1 & 3-1
+### Subtitle time  labels for visuals
+time_labels_change <- c("(2021-2040) - (2001-2020)", "(2041-2060) - (2001-2020)")
+
+### Map Change in Precipitation
+#### Map with legend first to call later separately
+sub_legend <- map(c(subtracted1[plot_model], 
+                    subtracted2[plot_model]), 
+                  function(p) {
+                    ggplot() + 
+                      geom_stars(data = p) +
+                      scale_fill_viridis_c(option = "D", na.value = "transparent") +
+                      coord_equal() +
+                      labs(fill = "Precipitation") +
+                      theme(legend.position = "bottom") +
+                      theme(legend.key.width = unit(1.5, 'cm'))
+                  })
+
+#### Map without legend that will later have single legend added
+sub_nolegend <- map(c(subtracted1[plot_model], 
+                      subtracted2[plot_model]), 
+                    function(p) {
+                      ggplot() + 
+                        borders("world", fill = "gray", colour = "black") +
+                        geom_stars(data = p) +
+                        scale_fill_viridis_c(option = "D", na.value = "transparent") +
+                        coord_equal(xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
+                        theme_light() +
+                        theme(legend.position = "none")
+                    })
+
+#### Add time ranges as labels to no legend map
+p_sub_nolegend <- plot_grid(plotlist = sub_nolegend, 
+                            nrow = 1, 
+                            ncol = 2, 
+                            labels = time_labels_change,
+                            label_size = 12) 
+
+#### Add legend and modify position
+p_sub_legend <- plot_grid(p_sub_nolegend + theme(legend.position = "none"), 
+                          get_legend(sub_legend[[1]] + theme(legend.box.margin = margin(0, 0, 0, 0))),
+                          rel_heights = c(0.9, 0.1), 
+                          nrow = 2, 
+                          ncol = 1)
+
+#### Create new ggplot with overarching title and subtitle
+title_gg2 <- ggplot() + 
+  labs(title = "Change (mm) in Precipitation", subtitle = str_glue("Model: {plot_model}")) 
+
+#### Final plot for Change in Precipitation
+sub_map <- plot_grid(title_gg2, p_sub_legend, ncol = 1, rel_heights = c(0.1, 0.75))
+sub_map
+
+
+## Percent Change in Precipitation: 2-1 & 3-1
+
+### Map Percent Change in Precipitation
+#### Map with legend first to call later separately
+per_legend <- map(c(perchangeim1[plot_model], 
+                    perchangeim2[plot_model]), 
+                  function(p) {
+                    ggplot() + 
+                      geom_stars(data = p) +
+                      scale_fill_viridis_c(option = "D", na.value = "transparent") +
+                      coord_equal() +
+                      labs(fill = "Precipitation") +
+                      theme(legend.position = "bottom") +
+                      theme(legend.key.width = unit(1.5, 'cm'))
+                  })
+
+#### Map without legend that will later have single legend added
+per_nolegend <- map(c(perchangeim1[plot_model], 
+                      perchangeim2[plot_model]), 
+                    function(p) {
+                      ggplot() + 
+                        borders("world", fill = "gray", colour = "black") +
+                        geom_stars(data = p) +
+                        scale_fill_viridis_c(option = "D", na.value = "transparent") +
+                        coord_equal(xlim = c(-95.1, -89.4), ylim = c(13.5, 19.0)) +
+                        theme_light() +
+                        theme(legend.position = "none")
+                    })
+
+#### Add time ranges as labels to no legend map
+p_per_nolegend <- plot_grid(plotlist = per_nolegend, 
+                            nrow = 1, 
+                            ncol = 2, 
+                            labels = time_labels_change,
+                            label_size = 12) 
+
+#### Add legend and modify position
+p_per_legend <- plot_grid(p_per_nolegend + theme(legend.position = "none"), 
+                          get_legend(per_legend[[1]] + theme(legend.box.margin = margin(0, 0, 0, 0))),
+                          rel_heights = c(0.9, 0.1), 
+                          nrow = 2, 
+                          ncol = 1)
+
+#### Create new ggplot with overarching title and subtitle
+title_gg3 <- ggplot() + 
+  labs(title = "Change (%) in Precipitation", subtitle = str_glue("Model: {plot_model}")) 
+
+#### Final plot for Percent Change in Precipitation
+per_map <- plot_grid(title_gg3, p_per_legend, ncol = 1, rel_heights = c(0.1, 0.75))
+per_map
+
